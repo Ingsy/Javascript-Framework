b81baf77ea87a198016e82584403fa19f2c46407
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-07 13:25:53 +0200

    Update App.js

--- javascript-framework/src/App.js
+++ javascript-framework/src/App.js
@@ -2,17 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Products from './components/ProductPage';
 import Home from './components/Homepage';
-import RouteNotFound from './components/Layout/RouteNotFound/RouteNotFound';
-
-
-
+import RouteNotFound from './components/Layout/RouteNotFound';
 
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="./components/Layout/index.jsx" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="products" element={<Products />} />
           <Route path="*" element={<RouteNotFound />} />
