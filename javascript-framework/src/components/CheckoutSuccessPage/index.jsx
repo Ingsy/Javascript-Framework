63daecf079fb3aea7c98a11b2176fa94471dd733
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-07 12:43:34 +0200

    CheckoutSuccess

---
+++ javascript-framework/src/components/CheckoutSuccessPage/index.jsx
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from './cartContext'; 
+
+const CheckoutSuccess = () => {
+  const { clearCart } = useContext(CartContext);
+
+  useEffect(() => {
+    clearCart();
+  }, [clearCart]);
+
+  return (
+    <div className="checkout-success">
+      <h1>Thank you for your order!</h1>
+      <p>Your order has been successfully processed.</p>
+      <li className="nav-item">
+        <Link to="/Homepage">Return to the Store</Link>
+      </li>
+    </div>
+  );
+};
+
+export default CheckoutSuccess;
