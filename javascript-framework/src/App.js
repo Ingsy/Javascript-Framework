1e6187373c8fb3da8196c97130d508791ae376b2
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-08 12:20:00 +0200

    testing routes

--- javascript-framework/src/App.js
+++ javascript-framework/src/App.js
@@ -4,15 +4,15 @@ import Products from './components/ProductPage';
 import Home from './components/Homepage';
 import RouteNotFound from './components/Layout/RouteNotFound';
 
-
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="./components/Layout/index.jsx" element={<Layout />}>
+        {/* Define a root route for the homepage */}
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="products" element={<Products />} />
-          <Route path="*" element={<RouteNotFound />} />
+          <Route path="*" element={<RouteNotFound />} /> {/* Catch-all route */}
         </Route>
       </Routes>
     </div>
