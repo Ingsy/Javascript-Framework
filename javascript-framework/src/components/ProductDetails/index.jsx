2ea515891c5f022fc4566c2b6426b78178d4e5aa
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-12 11:44:33 +0200

    PrroductDetails with/without reviews

--- javascript-framework/src/components/ProductDetails/index.jsx
+++ javascript-framework/src/components/ProductDetails/index.jsx
@@ -1,44 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true); // Add loading state
+  const [error, setError] = useState(null); // Add error state
 
   useEffect(() => {
     // Fetch product data using the product ID from the URL
     fetch(`https://api.noroff.dev/api/v1/online-shop/${id}`)
       .then((response) => response.json())
-      .then((data) => setProduct(data))
-      .catch((error) => console.error('Error fetching product data:', error));
+      .then((data) => {
+        setProduct(data);
+        setLoading(false); // Set loading to false when data is received
+      })
+      .catch((error) => {
+        setError(error); // Set error state if there's an error
+        setLoading(false); // Set loading to false in case of error
+      });
   }, [id]);
 
-  if (!product) {
+  if (loading) {
     return <p>Loading...</p>;
   }
 
-  const discount = product.discountedPrice ? product.price - product.discountedPrice : 0;
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (!product) {
+    return <p>Product not found.</p>;
+  }
+
+  const productPrice = product.price || 0;
+
+  const discount = product.discountedPrice
+    ? productPrice - product.discountedPrice
+    : 0;
 
   return (
-    <div className='container'>
-      <h2 className='mt-3'>{product.title}</h2>
-       <div className='card mt-3'>
-       <div className="card-body">
+    <div className="container">
+      <h2 className="mt-3">{product.title}</h2>
+      <div className="card mt-3">
+        <div className="card-body">
           {product.discountedPrice ? (
             <div>
+              <img src={product.imageUrl} alt={product.title} />
               <p className="card-text">
-                <del>${product.price.toFixed(2)}</del>
-                <span className="ml-2">${product.discountedPrice.toFixed(2)}</span>
+                <span className="ml-2">
+                  ${product.discountedPrice.toFixed(2)}
+                </span>
               </p>
               <p className="card-text text-success">
                 Save ${discount.toFixed(2)}
               </p>
             </div>
           ) : (
-            <p className="card-text">${product.price.toFixed(2)}</p>
+            <p className="card-text">${productPrice.toFixed(2)}</p>
           )}
-      {product.description}
-      <img src={product.img} alt="" />
+          <p className="card-text">Description: {product.description}</p>
+          {product.reviews && product.reviews.length > 0 ? (
+            <div>
+              <h3>Reviews:</h3>
+              <ul>
+                {product.reviews.map((review) => (
+                  <li key={review.id}>
+                    <strong>{review.username}</strong>
+                    <br />
+                    Rating: {review.rating}
+                    <br />
+                    {review.description}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>No reviews yet</p>
+          )}
         </div>
       </div>
     </div>
