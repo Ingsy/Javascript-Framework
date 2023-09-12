9a01ea83bb82b60420f4fdf1cd41cb6ba330e55b
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-12 12:59:23 +0200

    createRoot update

--- javascript-framework/src/index.js
+++ javascript-framework/src/index.js
@@ -1,42 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
+const root = createRoot(document.getElementById('root'));
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
-
-<BrowserRouter>
-  <nav>
-    <ul>
-      <li>
-        <Link to="/Home">Home</Link>
-      </li>
-      <li>
-        <Link to="/products">Products</Link>
-      </li>
-    </ul>
-  </nav>
-  <Routes>
-    <Route index element={<div>Home</div>} />
-    <Route path="products" element={<div>Products</div>} />
-    <Route path="*" element={<div>Route not found</div>} />
-  </Routes>
-</BrowserRouter>
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
