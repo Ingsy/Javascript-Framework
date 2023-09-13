a005a98d9d6721cc217e75d2f1a88aa359f037dc
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-13 10:30:45 +0200

    Update index.jsx

--- javascript-framework/src/components/CheckoutSuccessPage/index.jsx
+++ javascript-framework/src/components/CheckoutSuccessPage/index.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { CartContext } from './cartContext'; 
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "./cartContext";
 
 const CheckoutSuccess = () => {
   const { clearCart } = useContext(CartContext);
