e8ae0629661e6eabb618de8267f3576a1c22e2c7
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-13 13:12:00 +0200

    Update cartContext.jsx

--- javascript-framework/src/components/Cart/cartContext.jsx
+++ javascript-framework/src/components/Cart/cartContext.jsx
@@ -1,19 +1,48 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
+  const addToCart = (product) => {
+    // Check if the product already exists in the cart
+    const existingProductIndex = cart.findIndex(
+      (item) => item.id === product.id
+    );
+
+    if (existingProductIndex !== -1) {
+      // If the product already exists, update its quantity
+      const updatedCart = [...cart];
+      updatedCart[existingProductIndex].quantity += 1;
+
+      setCart(updatedCart);
+    } else {
+      // If the product doesn't exist, add it to the cart
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (productId) => {
+    // Remove a product from the cart
+    const updatedCart = cart.filter((item) => item.id !== productId);
+    setCart(updatedCart);
+  };
+
   const clearCart = () => {
     // Clear the cart by setting it to an empty array
     setCart([]);
   };
 
-  // Other cart-related functions (add to cart, remove from cart, etc.)
+  // Calculate the total price of items in the cart
+  const total = cart.reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+  }, 0);
 
   return (
-    <CartContext.Provider value={{ cart, clearCart }}>
+    <CartContext.Provider
+      value={{ cart, addToCart, removeFromCart, clearCart, total }}
+    >
       {children}
     </CartContext.Provider>
   );
