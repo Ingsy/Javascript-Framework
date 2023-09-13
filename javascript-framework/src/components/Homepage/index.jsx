320f683f2cd87083a797fd933e1a79239ae88e1f
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-13 10:30:17 +0200

    Adding responsive cols

--- javascript-framework/src/components/Homepage/index.jsx
+++ javascript-framework/src/components/Homepage/index.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import Layout from '../Layout';
-import ProductList from '../ProductList'; // Import your product data array
-import ProductDetails from '../ProductDetails';
+import React, { useEffect, useState } from "react";
+import ProductDetails from "../ProductDetails";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.noroff.dev/api/v1/online-shop`)
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error(`Error fetching products:`, error));
+  }, []);
+
   return (
-    <div>
-      <Layout />
-      <div className="container mt-5">
-        <h1>Welcome to Our Online Store</h1>
-        {ProductList.map((product) => (
-          <ProductDetails key={product.id} product={product} />
+    <div className="container mt-5">
+      <h1>Welcome to Our Online Store</h1>
+      <div className="row">
+        {products.map((product) => (
+          <div className="col-lg-4 col-md-6 col-sm-12" key={product.id}>
+            <ProductDetails product={product} />
+          </div>
         ))}
       </div>
     </div>
