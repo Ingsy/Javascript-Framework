867b02674205039d1258090f46b97d5d8232423f
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-15 11:49:36 +0200

    Update index.jsx

--- javascript-framework/src/components/ProductList/index.jsx
+++ javascript-framework/src/components/ProductList/index.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom';
+function ProductList() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const productsPerPage = 20;
 
-function ProductList({ products }) {
+  useEffect(() => {
+    // Fetch products only when the page changes
+    fetch(
+      `https://api.noroff.dev/api/v1/online-shop?page=${page}&items=${productsPerPage}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, [page]);
+
   return (
-    <div>
-      <h2>Welcome to Our online store</h2>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <Link to={`/product/${product.id}`}>{product.title}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className="row">
+      {products.map((product) => (
+        <div className="col-lg-4 col-md-6 col-sm-12" key={product.id}>
+          <div className="card mb-4">
+            <img
+              src={product.imageUrl}
+              alt={product.title}
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{product.title}</h5>
+              <p className="card-text">${product.price.toFixed(2)}</p>
+              <Link to={`/product/${product.id}`} className="btn btn-primary">
+                View Details
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+      <div className="pagination">
+        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+          Previous
+        </button>
+        <span>Page {page}</span>
+        <button onClick={() => setPage(page + 1)}>Next</button>
+      </div>
     </div>
   );
 }
