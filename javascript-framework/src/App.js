c9e679c74d9203bdc9473c720d6bc71ee7864c7a
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-19 08:17:25 +0200

    Update App.js

--- javascript-framework/src/App.js
+++ javascript-framework/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Homepage';
-import Products from './components/ProductPage';
 import RouteNotFound from './components/Layout/RouteNotFound';
 import CheckoutPage from "./components/CheckoutPage";
 import CheckoutSuccessPage from "./components/CheckoutSuccessPage";
 import ContactPage from './components/ContactPage';
 import ProductDetails from './components/ProductDetails';
+import ProductPage from "./components/ProductPage";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       {/* Define a root route for the homepage */}
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="products" element={<Products />} />
+        <Route path="products" element={<ProductPage />} />
         <Route path="checkout" element={<CheckoutPage />} />
         <Route path="checkout-success" element={<CheckoutSuccessPage />} />
         <Route path="contact" element={<ContactPage />} />
