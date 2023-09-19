90f5a670c2ec3eaaf95bc6215b43df6153d0c121
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-19 08:18:11 +0200

    search update

--- javascript-framework/src/components/ProductPage/index.jsx
+++ javascript-framework/src/components/ProductPage/index.jsx
@@ -1,5 +1,39 @@
-function Products() {
-    return <div>Products</div>;
-  }
+import React, { useState, useEffect } from "react";
+import ProductList from "../ProductList";
+import Search from "../Search";
 
-  export default Products;
+function Products() {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.noroff.dev/api/v1/online-shop")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
+  const handleSearchChange = (query) => {
+    setSearchQuery(query);
+
+    const filtered = products.filter((product) =>
+      product.title.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <Search onSearch={handleSearchChange} />
+      <ProductList products={searchQuery ? filteredProducts : products} />
+    </div>
+  );
+}
+
+export default Products;
