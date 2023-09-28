05fbabc9be1ce2fff019eb24bbf0516227e947a1
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-09-28 11:49:06 +0200

    cleanup -

--- javascript-framework/src/components/Buttons/index.jsx
+++ javascript-framework/src/components/Buttons/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Buttons.module.css";
 
 function BaseButton({ className, type, onClick, children }) {
-  const buttonClass = `${styles.BaseButton} ${className}`; // Use styles
+  const buttonClass = `${styles.BaseButton} ${className}`;
 
   return (
     <button
