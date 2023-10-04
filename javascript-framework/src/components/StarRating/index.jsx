696775647854705f2a830ed8a3d6e1f61e2e8a1b
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-10-04 15:10:29 +0200

    Update index.jsx

--- javascript-framework/src/components/StarRating/index.jsx
+++ javascript-framework/src/components/StarRating/index.jsx
@@ -4,21 +4,15 @@ import styles from "./star.module.css";
 function StarRating({ rating }) {
   const stars = [];
   for (let i = 1; i <= 5; i++) {
-    if (i <= rating) {
-      stars.push(
-        <span key={i} className={styles.starFilled}>
-          &#9733;
-        </span>
-      );
-    } else {
-      stars.push(
-        <span key={i} className={styles.starEmpty}>
-          &#9733;
-        </span>
-      );
-    }
+    stars.push(
+      <span
+        key={i}
+        className={i <= rating ? styles.starFilled : styles.starEmpty}
+      >
+        &#9733;
+      </span>
+    );
   }
-
   return <div className={styles.starRating}>{stars}</div>;
 }
 
