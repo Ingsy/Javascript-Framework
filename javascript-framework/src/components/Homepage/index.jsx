d280b5f55ec15d972315451a4ad17c642a2651bb
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-10-05 13:05:22 +0200

    Update index.jsx

--- javascript-framework/src/components/Homepage/index.jsx
+++ javascript-framework/src/components/Homepage/index.jsx
@@ -1,30 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ProductList from "../ProductList";
-import { useParams } from "react-router-dom";
 
-function Home() {
-  const [products, setProducts] = useState([]);
-  const { searchQuery } = useParams();
-
-  useEffect(() => {
-    fetch(`https://api.noroff.dev/api/v1/online-shop`)
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error(`Error fetching products:`, error));
-  }, []);
-
-  const filteredProducts = searchQuery
-    ? products.filter((product) =>
-        product.title.toLowerCase().includes(searchQuery.toLowerCase())
-      )
-    : products;
-
+function Home({ filterProd }) {
   return (
     <div className="container mt-3">
       <h1 className="text-center">Welcome to Our Online Store</h1>
       <div className="row">
         <div className="col">
-          <ProductList products={filteredProducts} />
+          <ProductList products={filterProd} />
         </div>
       </div>
     </div>
